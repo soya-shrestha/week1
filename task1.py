7263cb2b3e40afb55cd02dbb6fe4fe14d9e7c0dc
soya <soyashrestha91@gmail.com>
2022-10-11 11:58:28 +0545

    this is a commit

--- task1.py
+++ task1.py
@@ -1,6 +1,2 @@
 print("hello world")
-print("hello soya")
-print("temperatures in Yorkshire reached 38.4C.")
-fer=cel*9/5;
-cel=38.4;
-print("temperature in farenheit is :",fer)
+
---
+++ task3.py
@@ -0,0 +1,4 @@
+print("temperatures in Yorkshire reached 38.4C.")
+fer=cel*9/5;
+cel=38.4;
+print("temperature in farenheit is :",fer)
